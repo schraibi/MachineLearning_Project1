09313d238c2ed8be01d89d59b48a59b69f98104e
mthambipillai <melchior.thambipillai@epfl.ch>
2016-10-23 14:46:17 +0200

    split_data squash

--- code/data_tools/split_data.py
+++ code/data_tools/split_data.py
@@ -21,20 +21,3 @@ def split_data(x, y, ratio, seed=1):
     first_x = x[0:first_set_length]
     second_x = x[first_set_length:len(y)]
     return ((first_y, first_x),(second_y, second_x))
-
-
-
-x = np.array([
-    [2, 5, 3, 2.5 ],
-    [7, 1, 2, 1   ],
-    [0, 1, 3, 0.2 ]])
-
-y = [9,
-     8,
-     7]
-
-k_indices = [0,2]
-
-x2 = np.array([x[i,:] for i in k_indices])
-print(x2)
-print(split_data(x,y,0.4))
